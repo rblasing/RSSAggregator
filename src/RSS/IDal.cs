1c27471a83f58a37645dc70d6b8a7b1881d0b152
rblasing <80020050+rblasing@users.noreply.github.com>
2021-05-18 21:55:35 -0400

    Add files via upload

---
+++ src/RSS/IDal.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2020 Richard Blasingame. All rights reserved.
+
+using System;
+
+
+namespace RSS
+{
+   public interface IDal
+   {
+      FeedRow[] GetFeeds();
+      bool InsertFeed(string title, string url, bool isRegional);
+      bool DeleteFeed(int feedId);
+      bool EditFeed(int feedId, string title, string url, bool isRegional, bool isActive);
+      bool InsertItem(int feedId, Item item);
+      bool ItemExists(string url);
+      ItemRow[] GetTopItems(int count);
+      ItemRow[] GetNewItems(DateTime sinceDate);
+      DateTime GetMaxInsertDate();
+   }
+}
