1efbacf8141345df4cacb803edd461533bb3c02d
rblasing <80020050+rblasing@users.noreply.github.com>
2021-06-02 00:08:53 -0400

    Add files via upload

---
+++ src/RSSWeb/Controllers/ControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+
+
+namespace RSSWeb.Controllers
+{
+   public class ControllerBase : Controller
+   {
+      protected static readonly string SseUri = ConfigurationManager.AppSettings["sseUri"];
+
+      protected static readonly int HeatMapHours =
+         int.Parse((ConfigurationManager.AppSettings["heatMapHours"]));
+
+      protected static readonly string ConnStr =
+         ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString;
+
+      protected static readonly int MaxItems =
+         int.Parse((ConfigurationManager.AppSettings["viewMaxItems"]));
+
+      protected static readonly string XslUri = ConfigurationManager.AppSettings["xslUri"];
+   }
+}
---
+++ src/RSSWeb/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿// Copyright 2020 Richard Blasingame.All rights reserved.
+
+using System;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+
+namespace RSSWeb
+{
+   public class MvcApplication : System.Web.HttpApplication
+   {
+      protected void Application_Start()
+      {
+         AreaRegistration.RegisterAllAreas();
+         FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+         RouteConfig.RegisterRoutes(RouteTable.Routes);
+         BundleConfig.RegisterBundles(BundleTable.Bundles);
+      }
+
+
+      protected void Application_Error(object sender, EventArgs e)
+      {
+         var lastError = Server.GetLastError();
+
+         if (lastError != null)
+         {
+            log4net.LogManager.GetLogger("GlobalError").Error(lastError.Message);
+            log4net.LogManager.GetLogger("GlobalError").Error(lastError.StackTrace);
+
+            var baseError = lastError.GetBaseException();
+
+            if (baseError != null)
+            {
+               log4net.LogManager.GetLogger("GlobalError").Error(baseError.Message);
+               log4net.LogManager.GetLogger("GlobalError").Error(baseError.StackTrace);
+            }
+         }
+
+         Server.ClearError();
+      }
+
+
+      /// <summary>
+      /// This environment variable is guaranteed to be populated for Azure-
+      /// hosted web applications (as of 2021.5.22).
+      /// </summary>
+      /// <returns></returns>
+      public static bool IsAzureHosted()
+      {
+         return !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"));
+      }
+   }
+}
