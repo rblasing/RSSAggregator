c735b319e376d6a3c51ad5fe6ee467c1471360ec
rblasing <80020050+rblasing@users.noreply.github.com>
2021-06-02 22:46:43 -0400

    Add files via upload

---
+++ src/UtilTests/JsTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Util;
+
+
+namespace UtilTests
+{
+   [TestClass]
+   public class JsTest
+   {
+      [TestMethod]
+      public void DateFromJsTicksTest()
+      {
+         Assert.AreEqual(new DateTime(2021, 6, 1, 0, 0, 0, DateTimeKind.Utc),
+            Js.DateFromJsTicks(1622505600000));
+      }
+
+      
+      [TestMethod]
+      public void JsTicksFromDateTest()
+      {
+         Assert.AreEqual(1622505600000,
+            Js.JsTicksFromDate(new DateTime(2021, 6, 1, 0, 0, 0, DateTimeKind.Utc)));
+      }
+   }
+}
---
+++ src/UtilTests/UtilCoverageTest.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2020 Richard Blasingame.All rights reserved.
+
+using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Util;
+
+
+namespace UtilTests
+{
+   [TestClass]
+   public class UtilCoverageTest
+   {
+      [TestMethod]
+      public void CoverageTest()
+      {
+         Assembly testAssembly = this.GetType().Assembly;
+         Assembly targetAssembly = typeof(Util.Js).Assembly;
+
+         var errors = UnitTestCoverage.UnitTestCoverage.VerifyCoverage(targetAssembly, testAssembly);
+
+         if (errors.Count > 0)
+            Assert.Fail(string.Join("\r\n", errors));
+      }
+   }
+}
