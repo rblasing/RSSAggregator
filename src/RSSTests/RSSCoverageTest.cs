c00aec9e01f5d790fca7a79d978b9dbe638a53b5
rblasing <80020050+rblasing@users.noreply.github.com>
2021-06-06 23:27:43 -0400

    Add files via upload

---
+++ src/RSSTests/RSSCoverageTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+
+namespace RSSTests
+{
+   [TestClass]
+   public class RSSCoverageTest
+   {
+      [TestMethod]
+      public void CoverageTest()
+      {
+         Assembly testAssembly = this.GetType().Assembly;
+         Assembly targetAssembly = typeof(RSS.Channel).Assembly;
+
+         var errors = UnitTestCoverage.UnitTestCoverage.VerifyCoverage(targetAssembly, testAssembly);
+
+         if (errors.Count > 0)
+            Assert.Fail(string.Join("\r\n", errors));
+      }
+   }
+}
